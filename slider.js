e4de837ea968d0e48db79e78b6000ec5c2da9e33
OdunO <odunogunwuyi@gmail.com>
2017-07-29 11:20:13 +0100

    added new folders

---
+++ slider.js
@@ -0,0 +1,17 @@
+/**
+ * Created by seyi adeleke on 9/23/2016.
+ */
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel,10000); // Change image every 2 seconds
+}
